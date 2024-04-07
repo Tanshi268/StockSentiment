4770d887426f565a3126d48f58333177d44cd857
Tanshi Verma <127681947+Tanshi268@users.noreply.github.com>
2024-04-07 15:05:10 +0530

    Update 1_lstm_app.py

--- pages/1_lstm_app.py
+++ pages/1_lstm_app.py
@@ -8,6 +8,7 @@ from keras.layers import LSTM, Dense, Dropout
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.metrics import r2_score, mean_squared_error
 
+st.set_option('deprecation.showPyplotGlobalUse', False)
 st.set_page_config(page_title='StockSentiment - Analyzing the Stocks and Twitter Data', page_icon=':bar_chart:', layout='wide')
 
 # Load data
